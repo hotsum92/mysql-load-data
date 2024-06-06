76e66d4c97e058cdab01dbcfea2630d87af464ea
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-06-06 10:40:19 +0900

    update

---
+++ readme.md
@@ -0,0 +1,6 @@
+# mysql load data infile
+
+1. `./init-db.sh`
+2. `./load-data.sh`
+3. `./db.sh` and see the data
+
--- users.sql
+++ users.sql
@@ -1,3 +1,11 @@
+---- https://dev.classmethod.jp/articles/aurora-mysql-integrate-with-s3/
+--show variables like '%s3_role%';
+--select @@local_infile;
+--
+---- https://repost.aws/questions/QURXGWg0d2R7ii2YEZV_3BMA/aurora-mysql-8-load-from-s3-privileges-not-working
+--SELECT @@ROLE;
+--SET ROLE ALL;
+
 -- is local_infile enabled?
 -- select @@local_infile;
 
